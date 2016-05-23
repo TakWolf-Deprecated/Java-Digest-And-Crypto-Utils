509076b423ef83bded8a67078fa83fbe96033a98
TakWolf <takwolf@foxmail.com>
2016-05-23 21:27:40 +0800

    重新校验摘要部分

--- src/main/java/com/takwolf/app/demo/Main.java
+++ src/main/java/com/takwolf/app/demo/Main.java
@@ -16,34 +16,55 @@
 
 package com.takwolf.app.demo;
 
-import com.takwolf.util.codec.AES;
-import com.takwolf.util.codec.Base64;
-import com.takwolf.util.codec.DES3;
 import com.takwolf.util.codec.Digest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("----- Digest -----");
-        String plainText = "HelloWorld";
-        System.out.println("MD2:" + Digest.MD2.getMessage(plainText));
-        System.out.println("MD5:" + Digest.MD5.getMessage(plainText));
-        System.out.println("SHA1:" + Digest.SHA1.getMessage(plainText));
-        System.out.println("SHA256:" + Digest.SHA256.getMessage(plainText));
-        System.out.println("SHA384:" + Digest.SHA384.getMessage(plainText));
-        System.out.println("SHA512:" + Digest.SHA512.getMessage(plainText));
 
-        System.out.println("----- Crypto -----");
-        String text = "HelloWorld";
-        String key = "19B38D18-6CA2-8619-2424-D307381AFA11";
-        String strAes = AES.encrypt(key, text);
-        String strDes = DES3.encrypt(key, text);
-        System.out.println(strAes + " -> " + AES.decrypt(key, strAes));
-        System.out.println(strDes + " -> " + DES3.decrypt(key, strDes));
+        System.out.println("----- Digest MD5 -----");
 
-        System.out.println("----- Base64 -----");
-        System.out.println(Base64.encode("HelloWorld"));
-        System.out.println(Base64.decode("SGVsbG9Xb3JsZA=="));
+        Map<String, String> mapMD5 = new HashMap<>();
+        mapMD5.put("HelloWorld", "68e109f0f40ca72a15e05cc22786f8e6");
+        mapMD5.put("TakWolf", "1ad3f9663cb9fb09d14f6119010f0f64");
+        mapMD5.put("Google", "8b36e9207c24c76e6719268e49201d94");
+        mapMD5.put("今天的风儿有点喧嚣", "bf4fef9f779d426de14d26f406aafc30");
+        mapMD5.put("おはよう", "896c87c2017e74cf8c7ccf42d339ca66");
+        mapMD5.put("http://takwolf.com", "d5123f1e7c74c73a35fadc60fa92f011");
+
+        for (String key : mapMD5.keySet()) {
+            String ccValue = mapMD5.get(key);
+            String myValue = Digest.MD5.getMessage(key);
+            if (ccValue.equalsIgnoreCase(myValue)) {
+                System.out.println(key + " : " + ccValue);
+            } else {
+                throw new RuntimeException("digest error : " + key + "\nc : " + ccValue + "\nm : " + myValue);
+            }
+        }
+
+        System.out.println("----- Digest SHA256 -----");
+
+        Map<String, String> mapSHA256 = new HashMap<>();
+        mapSHA256.put("HelloWorld", "872e4e50ce9990d8b041330c47c9ddd11bec6b503ae9386a99da8584e9bb12c4");
+        mapSHA256.put("TakWolf", "b7fc98718fd0471db6d076794e10ae565421765db8f7e0fbcde9255c9799ceea");
+        mapSHA256.put("Google", "ce770667e5f9b0d8f55367bb79419689d90c48451bb33f079f3a9a72ae132de8");
+        mapSHA256.put("今天的风儿有点喧嚣", "9dd3eb3e3c6a0293f8fd15ba821b0e82e02f5dd93401fbf2401eda3a86101ff6");
+        mapSHA256.put("おはよう", "4314662c3426676a1dd586be6a6f1bddf8dae143c7e8514c50af525f9dc6cec8");
+        mapSHA256.put("http://takwolf.com", "5fa3b877455bb494d8ef136126bfd25b21800fe205951ae872f87f0df6040df4");
+
+        for (String key : mapSHA256.keySet()) {
+            String ccValue = mapSHA256.get(key);
+            String myValue = Digest.SHA256.getMessage(key);
+            if (ccValue.equalsIgnoreCase(myValue)) {
+                System.out.println(key + " : " + ccValue);
+            } else {
+                throw new RuntimeException("digest error : " + key + "\nc : " + ccValue + "\nm : " + myValue);
+            }
+        }
+
     }
 
 }
