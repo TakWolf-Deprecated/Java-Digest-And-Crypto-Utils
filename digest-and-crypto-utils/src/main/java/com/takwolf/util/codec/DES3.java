929989d160c9af54fd5eb7df3efe8980eca9a6aa
TakWolf <takwolf@foxmail.com>
2016-05-26 17:24:51 +0800

    调整des3实现

--- digest-and-crypto-utils/src/main/java/com/takwolf/util/codec/DES3.java
+++ digest-and-crypto-utils/src/main/java/com/takwolf/util/codec/DES3.java
@@ -32,11 +32,18 @@ public final class DES3 {
     private DES3() {}
 
     private static final Charset CHARSET_UTF_8 = Charset.forName("UTF-8");
+    private static final SecretKeyFactory secretKeyFactory;
+
+    static {
+        try {
+            secretKeyFactory = SecretKeyFactory.getInstance("DESede");
+        } catch (NoSuchAlgorithmException e) {
+            throw new AssertionError(e);
+        }
+    }
 
     public static String encrypt(String key, String iv, String data) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
-        DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(key.getBytes(CHARSET_UTF_8));
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
-        Key desKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+        Key desKey = secretKeyFactory.generateSecret(new DESedeKeySpec(key.getBytes(CHARSET_UTF_8)));
         IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes(CHARSET_UTF_8));
         Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, desKey, ivParameterSpec);
@@ -44,18 +51,14 @@ public final class DES3 {
     }
 
     public static String encrypt(String key, String data) throws InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException, InvalidAlgorithmParameterException, BadPaddingException, IllegalBlockSizeException {
-        DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(key.getBytes(CHARSET_UTF_8));
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
-        Key desKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+        Key desKey = secretKeyFactory.generateSecret(new DESedeKeySpec(key.getBytes(CHARSET_UTF_8)));
         Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, desKey);
         return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes(CHARSET_UTF_8)));
     }
 
     public static String decrypt(String key, String iv, String data) throws BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
-        DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(key.getBytes(CHARSET_UTF_8));
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
-        Key desKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+        Key desKey = secretKeyFactory.generateSecret(new DESedeKeySpec(key.getBytes(CHARSET_UTF_8)));
         IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes(CHARSET_UTF_8));
         Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, desKey, ivParameterSpec);
@@ -63,9 +66,7 @@ public final class DES3 {
     }
 
     public static String decrypt(String key, String data) throws BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
-        DESedeKeySpec deSedeKeySpec = new DESedeKeySpec(key.getBytes(CHARSET_UTF_8));
-        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("DESede");
-        Key desKey = secretKeyFactory.generateSecret(deSedeKeySpec);
+        Key desKey = secretKeyFactory.generateSecret(new DESedeKeySpec(key.getBytes(CHARSET_UTF_8)));
         Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, desKey);
         return new String(cipher.doFinal(Base64.getDecoder().decode(data)), CHARSET_UTF_8);
