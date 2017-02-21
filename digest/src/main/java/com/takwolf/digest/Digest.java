4a5db4280668bd80a1c6424bf9d40f2024ac1a7e
TakWolf <takwolf@foxmail.com>
2017-02-21 17:11:18 +0800

    调整摘要工具实现

--- digest/src/main/java/com/takwolf/digest/Digest.java
+++ digest/src/main/java/com/takwolf/digest/Digest.java
@@ -5,22 +5,48 @@ import java.security.NoSuchAlgorithmException;
 
 public final class Digest {
 
-    public static final Digest MD2 = new Digest("MD2");
-    public static final Digest MD5 = new Digest("MD5");
-    public static final Digest SHA1 = new Digest("SHA-1");
-    public static final Digest SHA256 = new Digest("SHA-256");
-    public static final Digest SHA384 = new Digest("SHA-384");
-    public static final Digest SHA512 = new Digest("SHA-512");
+    public static final Digest MD2 = new Digest(Algorithm.MD2);
+    public static final Digest MD5 = new Digest(Algorithm.MD5);
+    public static final Digest SHA1 = new Digest(Algorithm.SHA1);
+    public static final Digest SHA256 = new Digest(Algorithm.SHA256);
+    public static final Digest SHA384 = new Digest(Algorithm.SHA384);
+    public static final Digest SHA512 = new Digest(Algorithm.SHA512);
 
-    private final String algorithm;
+    public enum Algorithm {
 
-    private Digest(String algorithm) {
+        MD2("MD2"),
+
+        MD5("MD5"),
+
+        SHA1("SHA-1"),
+
+        SHA256("SHA-256"),
+
+        SHA384("SHA-384"),
+
+        SHA512("SHA-512");
+
+        private final String value;
+
+        Algorithm(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+    }
+
+    private final Algorithm algorithm;
+
+    private Digest(Algorithm algorithm) {
         this.algorithm = algorithm;
     }
 
     public byte[] getRaw(byte[] data) {
         try {
-            return MessageDigest.getInstance(algorithm).digest(data);
+            return MessageDigest.getInstance(algorithm.getValue()).digest(data);
         } catch (NoSuchAlgorithmException e) {
             throw new AssertionError(e);
         }
