a63469798fcf710460caac8e33e4aa48380da165
TakWolf <takwolf@foxmail.com>
2017-02-22 12:31:01 +0800

    增加签名算法测试用例

--- crypto/src/test/java/com/takwolf/crypto/RSATest.java
+++ crypto/src/test/java/com/takwolf/crypto/RSATest.java
@@ -14,19 +14,29 @@ public class RSATest {
     private static final String TEXT = "Hello World!";
 
     @Test
-    public void testRSA() throws NoSuchAlgorithmException, CryptoException {
+    public void testCrypto() throws NoSuchAlgorithmException, CryptoException {
         KeyPair keyPair = RSA.generateKeyPair(2048);
-        byte[] crypto = RSA.encryptByPublicKey(keyPair.getPublic(), TEXT.getBytes(StandardCharsets.UTF_8));
-        Assert.assertEquals(new String(RSA.decryptByPrivateKey(keyPair.getPrivate(), crypto), StandardCharsets.UTF_8), TEXT);
+        byte[] crypto = RSA.encrypt(keyPair.getPublic(), TEXT.getBytes(StandardCharsets.UTF_8));
+        String text = new String(RSA.decrypt(keyPair.getPrivate(), crypto), StandardCharsets.UTF_8);
+        Assert.assertEquals(text, TEXT);
     }
 
     @Test
-    public void testKey() throws NoSuchAlgorithmException, CryptoException {
+    public void testCryptoGenerateKey() throws NoSuchAlgorithmException, CryptoException {
         KeyPair keyPair = RSA.generateKeyPair(2048);
         PrivateKey privateKey = RSA.generatePrivateKey(keyPair.getPrivate().getEncoded());
         PublicKey publicKey = RSA.generatePublicKey(keyPair.getPublic().getEncoded());
-        byte[] crypto = RSA.encryptByPublicKey(publicKey, TEXT.getBytes(StandardCharsets.UTF_8));
-        Assert.assertEquals(new String(RSA.decryptByPrivateKey(privateKey, crypto), StandardCharsets.UTF_8), TEXT);
+        byte[] crypto = RSA.encrypt(publicKey, TEXT.getBytes(StandardCharsets.UTF_8));
+        String text = new String(RSA.decrypt(privateKey, crypto), StandardCharsets.UTF_8);
+        Assert.assertEquals(text, TEXT);
+    }
+
+    @Test
+    public void testSignVerify() throws CryptoException {
+        KeyPair keyPair = RSA.generateKeyPair(2048);
+        byte[] sign = RSA.sign(RSA.SignatureAlgorithm.SHA256, keyPair.getPrivate(), TEXT.getBytes(StandardCharsets.UTF_8));
+        boolean result = RSA.verify(RSA.SignatureAlgorithm.SHA256, keyPair.getPublic(), TEXT.getBytes(StandardCharsets.UTF_8), sign);
+        Assert.assertTrue(result);
     }
 
 }
