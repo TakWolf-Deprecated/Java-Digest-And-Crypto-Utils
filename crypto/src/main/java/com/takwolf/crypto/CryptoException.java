5541ebf137f68e86e0dd289a02bc2ca3201078d5
TakWolf <takwolf@foxmail.com>
2017-08-12 15:50:59 +0800

    代码整理

--- crypto/src/main/java/com/takwolf/crypto/CryptoException.java
+++ crypto/src/main/java/com/takwolf/crypto/CryptoException.java
@@ -16,8 +16,4 @@ public class CryptoException extends Exception {
         super(cause);
     }
 
-    public CryptoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
 }
